7ab925b63d6c1e6ecf9c7e8d882446923f60a95c
soyotroevin <soyotrokevin@gmail.com>
2022-09-19 09:36:58 -0300

    cambios menores

--- AppRegistro/forms.py
+++ AppRegistro/forms.py
@@ -3,20 +3,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from captcha.fields import CaptchaField
 
+# Lista con los años seleccionables en el registro
+mayor_edad = ['1980', '1981', '1982']
 
 class UserRegisterForm(UserCreationForm):
     password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repetir contraseña', widget=forms.PasswordInput)
-    birth_date = forms.DateField(label='Fecha de nacimiento', widget=forms.SelectDateWidget)
+    birth_date = forms.DateField(label='Fecha de nacimiento', widget=forms.SelectDateWidget(years=mayor_edad))
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'birth_date' ,'username', 'email', 'password1', 'password2']
 
-BIRTH_YEAR_CHOICES = ['1980', '1981', '1982']
 
 class UserUpdateForm(forms.ModelForm):
-    birth_date = forms.DateField(label='Fecha de nacimiento', widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
+    birth_date = forms.DateField(label='Fecha de nacimiento', widget=forms.SelectDateWidget(years=mayor_edad))
     class Meta:
         model = User
         fields = ['first_name','last_name', 'birth_date' ,'email']
